d959294907fa4c35fb82dffb4f294e9b621e265c
Sjors Driessen <sjors.driessen@outlook.com>
2024-01-26 16:34:57 +0100

    JS onclick implementation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let generate = document.getElementById('buttonGenerate');
+
+
+function buttonClick1() {
+    let txt1 = document.getElementById('bedrijfsnaam');
+    console.log('bedrijfsnaam: '+txt1.value)
+}
