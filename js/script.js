371df6dd27bda950920cd075ec31e7710cf7d721
Sjors Driessen <sjors.driessen@outlook.com>
2024-01-31 16:15:35 +0100

    Generate buttons onclick active state check implemented

--- js/script.js
+++ js/script.js
@@ -1,7 +1,61 @@
 let generate = document.getElementById('buttonGenerate');
 
 
+
+// document.getElementById('buttonOnderwerp').contains('action') = function() {
+//     alert("button was clicked");
+//  }​;​
+
+
 function buttonClick1() {
-    let txt1 = document.getElementById('bedrijfsnaam');
-    console.log('bedrijfsnaam: '+txt1.value)
+
+
+    let buttonOnderwerp = document.getElementById('buttonOnderwerp');
+    let buttonCTA = document.getElementById('buttonCTA');
+    let buttonSnippet = document.getElementById('buttonSnippet');
+    let buttonCopy = document.getElementById('buttonCopy');
+
+    if (buttonOnderwerp.classList.contains('active')){
+        buttonOnderwerp = 'Onderwerpsregel'
+    }
+    else {
+        buttonOnderwerp = '';
+    }
+
+    if (buttonCTA.classList.contains('active')){
+        buttonCTA = 'CTA';
+    }
+    else {
+        buttonCTA = '';
+    }
+
+    if (buttonSnippet.classList.contains('active')){
+        buttonSnippet = 'Snippet';
+    }
+    else {
+        buttonSnippet = '';
+    }
+
+    if (buttonCopy.classList.contains('active')){
+       buttonCopy = 'Copy';
+    }
+    else {
+        buttonCopy = '';
+    }
+
+    // array misschien niet gemakkelijk -> morgen even aanpassen
+    let generateArray = [buttonOnderwerp, buttonCTA, buttonSnippet, buttonCopy];
+
+    // console.log('Ontwikkel de volgende content:buttonOnderwerp + buttonCTA + buttonSnippet + buttonCopy');
+    console.log(generateArray);
+
+
+    //test    
+    let bedrijfsnaam = document.getElementById('bedrijfsnaam');
+    let bedrijfsrol = document.getElementById('bedrijfsrol');
+
+
+
+    console.log('bedrijfsnaam: '+bedrijfsnaam.value)
+    console.log('bedrijfsrol: '+bedrijfsrol.value)
 }
